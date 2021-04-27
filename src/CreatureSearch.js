ca9e30e08ddc0304788af9b125f51f52c4a74af7
Tucker Hoog <tuckerhoog@tutanota.com>
2021-04-27 16:12:05 -0400

    add CreatureSearch.js with form and button JSX and CreatureSearch.css

---
+++ src/CreatureSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class CreatureSearch extends Component {
+  render() {
+    return (
+      <form className="CreatureSearch">
+
+        <button></button>
+
+      </form>
+    );
+  }
+}
